699846c5f9676f23be1f5f306a015b697d63e93a
Sam C <sam@xeue.uk>
2022-12-27 21:35:27 +0000

    Made it launch an electron window

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
 	'env': {
 		'browser': true,
+		'node': true,
 		'es2021': true
 	},
 	'extends': 'eslint:recommended',
@@ -25,7 +26,7 @@ module.exports = {
 		],
 		'semi': [
 			'error',
-			'never'
+			'always'
 		]
 	}
 }
